a3761aeeb35317abec1e5e55faad1569978b5524
ilyas ben <ilyasbenamara594@gmail.com>
2025-05-30 16:45:07 +0200

    e

--- src/supabaseclient.js
+++ src/supabaseclient.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qkxsuxdhdxpwmsnfaati.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+export const supabaseUrl = 'https://qkxsuxdhdxpwmsnfaati.supabase.co'
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFreHN1eGRoZHhwd21zbmZhYXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MDQxMjUsImV4cCI6MjA2NDA4MDEyNX0.NJLSfsal-gNDw271j2ZBnmvxDBD3dUI0QsoiiTOWszY'
 export const supabase = createClient(supabaseUrl, supabaseKey)
